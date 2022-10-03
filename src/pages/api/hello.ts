37ab96e5eae3b3e904b4edcb9fd8302ceb5d8564
Qiming-Liu <prossliu@gmail.com>
2022-10-04 03:30:26 +1030

    clean: gh-pages branch

--- src/pages/api/hello.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  // req.method
-  return res.status(200).json("Hello World");
-};
--- src/services/gql/uset.ts
+++
@@ -1,7 +0,0 @@
-import { gql } from "@apollo/client";
-
-export const GET_USER = gql`
-  query {
-    token(email: "vinod@test.com")
-  }
-`;
