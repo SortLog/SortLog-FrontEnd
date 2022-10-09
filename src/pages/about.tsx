8e3bc36167d6b85aa55eda408d5e83e147b38a0c
Lucas Xiao <smy240975417@gmail.com>
2022-10-09 22:03:35 +1030

    remove ThemeProvider

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -5,16 +5,15 @@ import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import * as React from "react";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { makeStyles } from "@mui/styles";
-import { yellow, green, red } from "@mui/material/colors";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "../theme";
 import { GlobalStyles, Link } from "@mui/material";
 import Avatar from "@mui/material/Avatar";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const About: NextPage = () => {
   return (
-    <ThemeProvider theme={theme ? themeLight : themeDark}>
+    <ThemeProvider theme={theme}>
       <Container maxWidth="lg" color="background">
         <div className="App">
           <header className="App-header">
@@ -69,7 +68,11 @@ const About: NextPage = () => {
                   elevation={12}
                 >
                   <Box paddingTop="50px" paddingLeft="70px">
-                    <Avatar alt="Lucas Alan" src="icon-test.jpg" sx={{ height: 139, width: 139 }} />
+                    <Avatar
+                      alt="Lucas Alan"
+                      src="/icon-test.jpg"
+                      sx={{ height: 139, width: 139 }}
+                    />
                     <Typography variant="h6" paddingTop="10px" fontWeight="bold" color="#777676">
                       Lucas Alan
                       <Link href="https://www.linkedin.com/in/mingen-xiao/" target="_blank" color="#949494" paddingLeft="10px">
@@ -89,7 +92,7 @@ const About: NextPage = () => {
                   elevation={12}
                 >
                   <Box paddingTop="50px" paddingLeft="70px">
-                    <Avatar alt="AAA BBB" src="icon-test.jpg" sx={{ height: 139, width: 139 }} />
+                    <Avatar alt="AAA BBB" src="/icon-test.jpg" sx={{ height: 139, width: 139 }} />
                     <Typography variant="h6" paddingTop="10px" fontWeight="bold" color="#777676">
                       AAA BBB
                       <Link href="https://www.linkedin.com/in/mingen-xiao/" target="_blank" color="#949494" paddingLeft="10px">
@@ -109,7 +112,7 @@ const About: NextPage = () => {
                   elevation={12}
                 >
                   <Box paddingTop="50px" paddingLeft="70px">
-                    <Avatar alt="CCC DDD" src="icon-test.jpg" sx={{ height: 139, width: 139 }} />
+                    <Avatar alt="CCC DDD" src="/icon-test.jpg" sx={{ height: 139, width: 139 }} />
                     <Typography variant="h6" paddingTop="10px" fontWeight="bold" color="#777676">
                       CCC DDD
                       <Link href="https://www.linkedin.com/in/mingen-xiao/" target="_blank" color="#949494" paddingLeft="10px">
@@ -129,7 +132,7 @@ const About: NextPage = () => {
                   elevation={12}
                 >
                   <Box paddingTop="50px" paddingLeft="70px">
-                    <Avatar alt="EEE FFF" src="icon-test.jpg" sx={{ height: 139, width: 139 }} />
+                    <Avatar alt="EEE FFF" src="/icon-test.jpg" sx={{ height: 139, width: 139 }} />
                     <Typography variant="h6" paddingTop="10px" fontWeight="bold" color="#777676">
                       EEE FFF
                       <Link href="https://www.linkedin.com/in/mingen-xiao/" target="_blank" color="#949494" paddingLeft="10px">
@@ -149,7 +152,7 @@ const About: NextPage = () => {
                   elevation={12}
                 >
                   <Box paddingTop="50px" paddingLeft="70px">
-                    <Avatar alt="GGG HHH" src="icon-test.jpg" sx={{ height: 139, width: 139 }} />
+                    <Avatar alt="GGG HHH" src="/icon-test.jpg" sx={{ height: 139, width: 139 }} />
                     <Typography variant="h6" paddingTop="10px" fontWeight="bold" color="#777676">
                       GGG HHH
                       <Link href="https://www.linkedin.com/in/mingen-xiao/" target="_blank" color="#949494" paddingLeft="10px">
@@ -165,7 +168,7 @@ const About: NextPage = () => {
               <Grid item>
                 <Paper style={{ height: 260, width: 280, borderRadius: 30 }} elevation={12}>
                   <Box paddingTop="50px" paddingLeft="70px">
-                    <Avatar alt="III JJJ" src="icon-test.jpg" sx={{ height: 139, width: 139 }} />
+                    <Avatar alt="III JJJ" src="/icon-test.jpg" sx={{ height: 139, width: 139 }} />
                     <Typography variant="h6" paddingTop="10px" fontWeight="bold" color="#777676">
                       III JJJ
                       <Link
@@ -186,7 +189,7 @@ const About: NextPage = () => {
               <Grid item>
                 <Paper style={{ height: 260, width: 280, borderRadius: 30 }} elevation={12}>
                   <Box paddingTop="50px" paddingLeft="70px">
-                    <Avatar alt="KKK LLL" src="icon-test.jpg" sx={{ height: 139, width: 139 }} />
+                    <Avatar alt="KKK LLL" src="/icon-test.jpg" sx={{ height: 139, width: 139 }} />
                     <Typography variant="h6" paddingTop="10px" fontWeight="bold" color="#777676">
                       KKK LLL
                       <Link
@@ -207,7 +210,7 @@ const About: NextPage = () => {
               <Grid item>
                 <Paper style={{ height: 260, width: 280, borderRadius: 30 }} elevation={12}>
                   <Box paddingTop="50px" paddingLeft="70px">
-                    <Avatar alt="MMM NNN" src="icon-test.jpg" sx={{ height: 139, width: 139 }} />
+                    <Avatar alt="MMM NNN" src="/icon-test.jpg" sx={{ height: 139, width: 139 }} />
                     <Typography variant="h6" paddingTop="10px" fontWeight="bold" color="#777676">
                       MMM NNN
                       <Link
@@ -228,7 +231,7 @@ const About: NextPage = () => {
               <Grid item>
                 <Paper style={{ height: 260, width: 280, borderRadius: 30 }} elevation={12}>
                   <Box paddingTop="50px" paddingLeft="70px">
-                    <Avatar alt="OOO PPP" src="icon-test.jpg" sx={{ height: 139, width: 139 }} />
+                    <Avatar alt="OOO PPP" src="/icon-test.jpg" sx={{ height: 139, width: 139 }} />
                     <Typography variant="h6" paddingTop="10px" fontWeight="bold" color="#777676">
                       OOO PPP
                       <Link
@@ -249,7 +252,7 @@ const About: NextPage = () => {
               <Grid item>
                 <Paper style={{ height: 260, width: 280, borderRadius: 30 }} elevation={12}>
                   <Box paddingTop="50px" paddingLeft="70px">
-                    <Avatar alt="QQQ RRR" src="icon-test.jpg" sx={{ height: 139, width: 139 }} />
+                    <Avatar alt="QQQ RRR" src="/icon-test.jpg" sx={{ height: 139, width: 139 }} />
                     <Typography variant="h6" paddingTop="10px" fontWeight="bold" color="#777676">
                       QQQ RRR
                       <Link
@@ -272,7 +275,6 @@ const About: NextPage = () => {
         </div>
       </Container>
     </ThemeProvider>
->>>>>>> 2754ee4 (create a new tsx file for about us page)
   );
 };
 
