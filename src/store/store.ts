f937cb61c66525066682f837ef19ddecdf66e00c
Lucas Xiao <smy240975417@gmail.com>
2022-10-26 00:35:45 +1030

    update information

--- src/store/store.ts
+++ src/store/store.ts
@@ -17,10 +17,10 @@ export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 export type AppThunk<ReturnType = void> = ThunkAction<
-ReturnType,
-RootState,
-unknown,
-Action<String>
+  ReturnType,
+  RootState,
+  unknown,
+  Action<String>
 >;
 
 export default store;
