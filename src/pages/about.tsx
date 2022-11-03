27c01fa29a9163626ce303b1e2d1f0d062f1f6c4
MINGEN XIAO <69286978+LucasssXiao@users.noreply.github.com>
2022-11-03 21:21:33 +1030

    Sor 69 add and edit detail of item (#38)
    * create a new tsx file for about us page
    
    * complete the structure of the issue About page
    
    * update about us
    
    * remove ThemeProvider
    
    * Update the use of images
    
    * item list page
    
    * resolve conflict
    
    * delete unused file
    
    * fix error
    
    * fix error
    
    * update information
    
    * correct the directory
    
    * item info
    
    Co-authored-by: Junyi Men <40311592+menjunyi@users.noreply.github.com>

---
+++ src/pages/about.tsx
@@ -0,0 +1,316 @@
+import type { NextPage } from "next";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import * as React from "react";
+import { GlobalStyles, Link } from "@mui/material";
+import Avatar from "@mui/material/Avatar";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
+const About: NextPage = () => {
+  return (
+    <>
+      <Container maxWidth="lg" color="background">
+        <div className="App">
+          <header className="App-header">
+            <GlobalStyles
+              styles={{
+                body: { backgroundColor: "#eeecec" },
+              }}
+            />
+            <Typography
+              variant="h1"
+              component="div"
+              fontWeight="bold"
+              color="#373b54"
+              align="center"
+              paddingTop="66px"
+            >
+              About <text style={{ color: "#cd1530" }}>Us</text>.
+            </Typography>
+            <Typography
+              fontSize="17.5px"
+              paddingLeft="390px"
+              paddingRight="390px"
+              paddingTop="33px"
+              color="#8c8989"
+              align="center"
+            >
+              Sortly is a software solution designed for businesses to streamline and modernize
+              every aspect of managing inventory.
+            </Typography>
+          </header>
+        </div>
+      </Container>
+
+      <Container maxWidth="xl">
+        <div className="App">
+          <header className="App-header">
+            <Typography
+              variant="h3"
+              component="div"
+              color="#373b54"
+              fontWeight="bold"
+              paddingTop="100px"
+            >
+              Sortlog <text style={{ color: "#cd1530" }}>Team</text>
+            </Typography>
+
+            <Grid container spacing={6} paddingTop="50px">
+              <Grid item>
+                <Paper style={{ height: 260, width: 280, borderRadius: 30 }} elevation={12}>
+                  <Box paddingTop="50px" paddingLeft="70px">
+                    <Avatar
+                      alt="Jenny Men"
+                      src="/png/icon-test.png"
+                      sx={{ height: 139, width: 139 }}
+                    />
+                    <Typography variant="h6" paddingTop="10px" fontWeight="bold" color="#777676">
+                      Jenny Men
+                      <Link
+                        href="www.linkedin.com/in/junyi-men"
+                        target="_blank"
+                        color="#949494"
+                        paddingLeft="10px"
+                      >
+                        <LinkedInIcon fontSize="small" />
+                      </Link>
+                    </Typography>
+                    <Typography fontSize="13px" color="#777676">
+                      Developer, Team Leader
+                    </Typography>
+                  </Box>
+                </Paper>
+              </Grid>
+              <Grid item>
+                <Paper style={{ height: 260, width: 280, borderRadius: 30 }} elevation={12}>
+                  <Box paddingTop="50px" paddingLeft="70px">
+                    <Avatar
+                      alt="Mingen Xiao"
+                      src="/png/mingen-xiao.png"
+                      sx={{ height: 139, width: 139 }}
+                    />
+                    <Typography variant="h6" paddingTop="10px" fontWeight="bold" color="#777676">
+                      Mingen Xiao
+                      <Link
+                        href="https://www.linkedin.com/in/mingen-xiao/"
+                        target="_blank"
+                        color="#949494"
+                        paddingLeft="10px"
+                      >
+                        <LinkedInIcon fontSize="small" />
+                      </Link>
+                    </Typography>
+                    <Typography fontSize="13px" color="#777676">
+                      Developer
+                    </Typography>
+                  </Box>
+                </Paper>
+              </Grid>
+              <Grid item>
+                <Paper style={{ height: 260, width: 280, borderRadius: 30 }} elevation={12}>
+                  <Box paddingTop="50px" paddingLeft="70px">
+                    <Avatar
+                      alt="Tony Liu"
+                      src="/png/tony-liu.png"
+                      sx={{ height: 139, width: 139 }}
+                    />
+                    <Typography variant="h6" paddingTop="10px" fontWeight="bold" color="#777676">
+                      Tony Liu
+                      <Link
+                        href="www.linkedin.com/in/tony-dong-liu"
+                        target="_blank"
+                        color="#949494"
+                        paddingLeft="10px"
+                      >
+                        <LinkedInIcon fontSize="small" />
+                      </Link>
+                    </Typography>
+                    <Typography fontSize="13px" color="#777676">
+                      Developer
+                    </Typography>
+                  </Box>
+                </Paper>
+              </Grid>
+              <Grid item>
+                <Paper style={{ height: 260, width: 280, borderRadius: 30 }} elevation={12}>
+                  <Box paddingTop="50px" paddingLeft="70px">
+                    <Avatar alt="Roy Qin" src="/png/roy-qin.png" sx={{ height: 139, width: 139 }} />
+                    <Typography variant="h6" paddingTop="10px" fontWeight="bold" color="#777676">
+                      Roy Qin
+                      <Link
+                        href="www.linkedin.com/in/roy-qin"
+                        target="_blank"
+                        color="#949494"
+                        paddingLeft="10px"
+                      >
+                        <LinkedInIcon fontSize="small" />
+                      </Link>
+                    </Typography>
+                    <Typography fontSize="13px" color="#777676">
+                      Developer
+                    </Typography>
+                  </Box>
+                </Paper>
+              </Grid>
+              <Grid item>
+                <Paper style={{ height: 260, width: 280, borderRadius: 30 }} elevation={12}>
+                  <Box paddingTop="50px" paddingLeft="70px">
+                    <Avatar
+                      alt="Kevin Luk"
+                      src="/png/icon-test.png"
+                      sx={{ height: 139, width: 139 }}
+                    />
+                    <Typography variant="h6" paddingTop="10px" fontWeight="bold" color="#777676">
+                      Kevin Luk
+                      <Link
+                        href="https://www.linkedin.com/in/mingen-xiao/"
+                        target="_blank"
+                        color="#949494"
+                        paddingLeft="10px"
+                      >
+                        <LinkedInIcon fontSize="small" />
+                      </Link>
+                    </Typography>
+                    <Typography fontSize="13px" color="#777676">
+                      Developer
+                    </Typography>
+                  </Box>
+                </Paper>
+              </Grid>
+              <Grid item>
+                <Paper style={{ height: 260, width: 280, borderRadius: 30 }} elevation={12}>
+                  <Box paddingTop="50px" paddingLeft="70px">
+                    <Avatar
+                      alt="Yuchen Diao"
+                      src="/png/yuchen-diao.png"
+                      sx={{ height: 139, width: 139 }}
+                    />
+                    <Typography variant="h6" paddingTop="10px" fontWeight="bold" color="#777676">
+                      Yuchen Diao
+                      <Link
+                        href="https://www.linkedin.com/in/yuchen-diao/"
+                        target="_blank"
+                        color="#949494"
+                        paddingLeft="10px"
+                      >
+                        <LinkedInIcon fontSize="small" />
+                      </Link>
+                    </Typography>
+                    <Typography fontSize="13px" color="#777676">
+                      DevOps
+                    </Typography>
+                  </Box>
+                </Paper>
+              </Grid>
+              <Grid item>
+                <Paper style={{ height: 260, width: 280, borderRadius: 30 }} elevation={12}>
+                  <Box paddingTop="50px" paddingLeft="70px">
+                    <Avatar
+                      alt="Shawn Wang"
+                      src="/png/shawn-wang.png"
+                      sx={{ height: 139, width: 139 }}
+                    />
+                    <Typography variant="h6" paddingTop="10px" fontWeight="bold" color="#777676">
+                      Shawn Wang
+                      <Link
+                        href="https://www.linkedin.com/in/tremendous-shawn-wang/"
+                        target="_blank"
+                        color="#949494"
+                        paddingLeft="10px"
+                      >
+                        <LinkedInIcon fontSize="small" />
+                      </Link>
+                    </Typography>
+                    <Typography fontSize="13px" color="#777676">
+                      DevOps
+                    </Typography>
+                  </Box>
+                </Paper>
+              </Grid>
+              <Grid item>
+                <Paper style={{ height: 260, width: 280, borderRadius: 30 }} elevation={12}>
+                  <Box paddingTop="50px" paddingLeft="70px">
+                    <Avatar
+                      alt="Sally Song"
+                      src="/png/sally-song.png"
+                      sx={{ height: 139, width: 139 }}
+                    />
+                    <Typography variant="h6" paddingTop="10px" fontWeight="bold" color="#777676">
+                      Sally Song
+                      <Link
+                        href="https://www.linkedin.com/in/sallysong30"
+                        target="_blank"
+                        color="#949494"
+                        paddingLeft="10px"
+                      >
+                        <LinkedInIcon fontSize="small" />
+                      </Link>
+                    </Typography>
+                    <Typography fontSize="13px" color="#777676">
+                      Devops
+                    </Typography>
+                  </Box>
+                </Paper>
+              </Grid>
+              <Grid item>
+                <Paper style={{ height: 260, width: 280, borderRadius: 30 }} elevation={12}>
+                  <Box paddingTop="50px" paddingLeft="70px">
+                    <Avatar
+                      alt="MMM NNN"
+                      src="/png/icon-test.png"
+                      sx={{ height: 139, width: 139 }}
+                    />
+                    <Typography variant="h6" paddingTop="10px" fontWeight="bold" color="#777676">
+                      MMM NNN
+                      <Link
+                        href="https://www.linkedin.com/in/mingen-xiao/"
+                        target="_blank"
+                        color="#949494"
+                        paddingLeft="10px"
+                      >
+                        <LinkedInIcon fontSize="small" />
+                      </Link>
+                    </Typography>
+                    <Typography fontSize="13px" color="#777676">
+                      Full-Stack Developer
+                    </Typography>
+                  </Box>
+                </Paper>
+              </Grid>
+              <Grid item>
+                <Paper style={{ height: 260, width: 280, borderRadius: 30 }} elevation={12}>
+                  <Box paddingTop="50px" paddingLeft="70px">
+                    <Avatar
+                      alt="OOO PPP"
+                      src="/png/icon-test.png"
+                      sx={{ height: 139, width: 139 }}
+                    />
+                    <Typography variant="h6" paddingTop="10px" fontWeight="bold" color="#777676">
+                      OOO PPP
+                      <Link
+                        href="https://www.linkedin.com/in/mingen-xiao/"
+                        target="_blank"
+                        color="#949494"
+                        paddingLeft="10px"
+                      >
+                        <LinkedInIcon fontSize="small" />
+                      </Link>
+                    </Typography>
+                    <Typography fontSize="13px" color="#777676">
+                      Full-Stack Developer
+                    </Typography>
+                  </Box>
+                </Paper>
+              </Grid>
+            </Grid>
+          </header>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default About;
---
+++ src/pages/dashboard/item-list.tsx
@@ -0,0 +1,327 @@
+import React, { useState } from "react";
+import type { NextPage } from "next";
+import Container from "@mui/material/Container";
+import Inventory2OutlinedIcon from "@mui/icons-material/Inventory2Outlined";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { Alert, Chip, Divider, GlobalStyles, Grid, Icon, Paper } from "@mui/material";
+import Button from "@mui/material/Button";
+import { Add } from "@mui/icons-material";
+import { Box } from "@mui/system";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
+import QrCodeScannerIcon from "@mui/icons-material/QrCodeScanner";
+import SouthIcon from "@mui/icons-material/South";
+import { DataGrid, GridToolbar, GridValueGetterParams } from "@mui/x-data-grid";
+import ImgMediaCard from "@/components/ItemList/table-card";
+import SplitButton from "@/components/ItemList/split-button";
+import MuiDrawer from "@/components/ItemList/add-and-edit";
+
+function moneyMapper(money: any) {
+  return parseFloat(money)
+    .toFixed(2)
+    .toString()
+    .split("")
+    .reverse()
+    .join("")
+    .replace(/(\d{3})/g, "$1,")
+    .replace(/\,$/, "")
+    .split("")
+    .reverse()
+    .join("");
+}
+
+const mockDataItemList = [
+  {
+    id: 1,
+    img: "https://source.unsplash.com/random",
+    name: "Item 1",
+    quantity: 1,
+    price: 100,
+    tag: ["tools", "ryobi"],
+  },
+  {
+    id: 2,
+    img: "https://source.unsplash.com/random",
+    name: "Item 2",
+    quantity: 123,
+    price: 200,
+    tag: ["tools", "ryobi"],
+  },
+  {
+    id: 3,
+    img: "https://source.unsplash.com/random",
+    name: "Item 3",
+    quantity: 2,
+    price: 300,
+    tag: ["ryobi"],
+  },
+];
+
+// const sort = mockDataItemList.name.sort()
+// console.log(sort)
+
+function getFullName(params: GridValueGetterParams) {
+  return `${params.row.name || ""} ${params.row.quantity || ""}`;
+}
+
+const columns = [
+  {
+    field: "name",
+    headerName: "Name",
+    //  renderCell: (params) => (dataCal(mockDataItemList).map((detail: any) => (
+    //   <MuiDrawer
+    //     isDrawerOpen={isDrawerOpen}
+    //     setIsDrawerOpen={setIsDrawerOpen}
+    //     data={detail}
+    //     key={detail.id}
+    //   />
+    // )))
+  },
+  { field: "quantity", headerName: "Quantity" },
+  { field: "price", headerName: "Price" },
+  { field: "tag", headerName: "Tag" },
+  {
+    field: "fullName",
+    headerName: "Full name",
+    width: 160,
+    valueGetter: getFullName,
+  },
+];
+
+const dataMapper = (rows: any, searchText: string) => {
+  return rows
+    .filter(
+      (row: any) =>
+        row.name.includes(searchText) || row.tag.find((t: string) => t.includes(searchText))
+    )
+    .map((row: any) => {
+      return {
+        ...row,
+        quantity: row.quantity.toString() + " unit",
+        price: "$" + moneyMapper(row.price),
+        // tag: row.tag.map((tag:any, index: number) => <Chip key={'chip${index}'} label={tag} />),
+      };
+    });
+};
+
+const dataCal = (rows: any) => {
+  return rows.map((row: any) => {
+    return {
+      ...row,
+    };
+  });
+};
+
+const ItemList: NextPage = () => {
+  const [isGrid, setIsGrid] = useState(false);
+  const [searchText, setSearchText] = useState("");
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  let x = 0;
+  let y = 0;
+  let z = 0;
+
+  const [message, setMessage] = React.useState("");
+  const handleRowClick = (params) => {
+    setIsDrawerOpen(true);
+
+    dataMapper(mockDataItemList, searchText).map((detail: any) => (
+      <MuiDrawer
+        isDrawerOpen={isDrawerOpen}
+        setIsDrawerOpen={setIsDrawerOpen}
+        data={params.row}
+        key={detail.id}
+      />
+    ));
+
+    setMessage(`Movie "${params.row.name}" clicked`);
+  };
+
+  return (
+    <>
+      <Container maxWidth="lg" color="background">
+        <div className="App">
+          <header className="App-header">
+            <GlobalStyles
+              styles={{
+                body: { backgroundColor: "#eeecec" },
+              }}
+            />
+            {/* {dataMapper(mockDataItemList, searchText).map((detail: any) => (
+              <MuiDrawer
+                isDrawerOpen={isDrawerOpen}
+                setIsDrawerOpen={setIsDrawerOpen}
+                data={detail}
+                key={detail.id}
+              />
+            ))} */}
+            <Grid container spacing={6} paddingTop="60px" justifyContent="space-between">
+              <Typography variant="h3" color="#6a6a6c" paddingLeft="76px">
+                <Inventory2OutlinedIcon
+                  fontSize="large"
+                  sx={{ marginRight: "10px", paddingTop: 1 }}
+                />
+                All Items
+              </Typography>
+              <Button
+                variant="contained"
+                startIcon={<Add />}
+                sx={{ backgroundColor: "#e70a3e" }}
+                onClick={() => setIsDrawerOpen(true)}
+              >
+                ADD NEW
+              </Button>
+            </Grid>
+            <Divider sx={{ color: "#d5cfcf", margin: 3, border: 1, width: 1129 }} />
+            <Grid
+              container
+              spacing={6}
+              paddingTop="50px"
+              paddingLeft="76px"
+              justifyContent="space-between"
+            >
+              <Paper
+                component="form"
+                sx={{
+                  maxHeight: "45px",
+                  maxWidth: "262px",
+                  display: "flex",
+                  alignItems: "center",
+                  width: 300,
+                }}
+              >
+                <Icon sx={{ paddingLeft: "6px", color: "#c2c0c0", width: 30 }}>
+                  <SearchIcon />
+                </Icon>
+                <InputBase
+                  sx={{ ml: 1, flex: 1 }}
+                  placeholder="Search All Items"
+                  inputProps={{ "aria-label": "search google maps" }}
+                  onChange={(e) => {
+                    setSearchText((e.target as any).value);
+                  }}
+                />
+                <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+                <IconButton sx={{ p: "10px", color: "#2d2a2a" }} aria-label="QrCodeScannerIcon">
+                  <QrCodeScannerIcon />
+                </IconButton>
+              </Paper>
+              <Grid>
+                {/* <Button
+                  variant="text"
+                  color="inherit"
+                  sx={{
+                    marginBottom: "13px",
+                    marginRight: "13px",
+                    ":hover": { backgroundColor: "#3a3939", color: "#ffffff" },
+                  }}
+                >
+                  Updated At
+                  <SouthIcon />
+                </Button> */}
+                <SplitButton setIsGrid={setIsGrid} sx={{ height: 100 }} />
+              </Grid>
+            </Grid>
+            {dataCal(mockDataItemList).map((value: any) => (
+              <Box sx={{ mt: 3, mr: 3 }} key={value.id}>
+                <Typography sx={{ display: "none" }}>
+                  {(x = x + 1)} {(y = y + value.quantity)} {(z = z + value.price)}
+                </Typography>
+              </Box>
+            ))}
+            {/* {dataCal(mockDataItemList).map((detail: any) => (
+              <Box sx={{ mt: 3, mr: 3 }} key={detail.id}>
+                aaa
+                <MuiDrawer
+                  isDrawerOpen={isDrawerOpen}
+                  setIsDrawerOpen={setIsDrawerOpen}
+                  data={mockDataItemList.name}
+                />
+                <Typography>{detail.item}</Typography>
+              </Box>
+            ))} */}
+            <Grid container spacing={6} pb="10px">
+              <Grid item color="#6a6a6c">
+                Folders:{" "}
+                <Grid sx={{ display: "inline" }} color="#393939" fontWeight="bold">
+                  0
+                </Grid>
+              </Grid>
+              <Grid item color="#6a6a6c">
+                Items:{" "}
+                <Grid sx={{ display: "inline" }} color="#393939" fontWeight="bold">
+                  {x}
+                </Grid>
+              </Grid>
+              <Grid item color="#6a6a6c">
+                Total Quantity:{" "}
+                <Grid sx={{ display: "inline" }} color="#393939" fontWeight="bold">
+                  {y} unit(s)
+                </Grid>
+              </Grid>
+              <Grid item color="#6a6a6c">
+                Total Value:{" "}
+                <Grid sx={{ display: "inline" }} color="#393939" fontWeight="bold">
+                  ${z}.00
+                </Grid>
+              </Grid>
+            </Grid>
+            <Box>
+              {isGrid ? (
+                <Grid container>
+                  {dataMapper(mockDataItemList, searchText).map((card: any) => (
+                    <Box sx={{ mt: 3, mr: 3 }} key={card.id}>
+                      {/* <Box sx={{ mt: 3, mr: 3 }} key={card.id} onClick={() => setIsDrawerOpen(true)}> */}
+                      <ImgMediaCard data={card} />
+                      {/* <Divider sx={{ color: "#d5cfcf", margin: 3, border: 1, width: 1129 }}/> */}
+                      {/* <MuiDrawer
+                        isDrawerOpen={isDrawerOpen}
+                        setIsDrawerOpen={setIsDrawerOpen}
+                        data={card}
+                      /> */}
+                    </Box>
+                  ))}
+                </Grid>
+              ) : (
+                <Box sx={{ height: 600, width: "100%", bgcolor: "#dadada" }}>
+                  {/* {(`Movie "${params.row.title}" clicked`)} */}
+                  {/* {dataMapper(mockDataItemList, searchText).map((detail: any) => (
+                    <MuiDrawer
+                      isDrawerOpen={isDrawerOpen}
+                      setIsDrawerOpen={setIsDrawerOpen}
+                      data={detail}
+                      key={detail.id}
+                    />
+                  ))} */}
+                  <DataGrid
+                    rows={dataMapper(mockDataItemList, searchText)}
+                    columns={columns}
+                    pageSize={5}
+                    rowsPerPageOptions={[5]}
+                    checkboxSelection
+                    disableSelectionOnClick
+                    onRowClick={handleRowClick}
+                    // onRowClick={() => setIsDrawerOpen(true)}
+                    // components={{ Toolbar: GridToolbar }}
+                    // experimentalFeatures={{ aggregation: true }}
+                  />
+                  {dataMapper(mockDataItemList, searchText).map((detail: any) => (
+                    <MuiDrawer
+                      isDrawerOpen={isDrawerOpen}
+                      setIsDrawerOpen={setIsDrawerOpen}
+                      data={detail}
+                      key={detail.id}
+                    />
+                  ))}
+                  {message && <Alert severity="info">{message}</Alert>}
+                </Box>
+              )}
+            </Box>
+          </header>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default ItemList;
