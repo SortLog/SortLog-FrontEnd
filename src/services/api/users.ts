5c6e444962f283e90c2db41cde850fe6a3440a5d
Junyi Men <40311592+menjunyi@users.noreply.github.com>
2022-11-15 18:24:56 +1100

    add user profile load and update (#50)

--- src/services/api/users.ts
+++ src/services/api/users.ts
@@ -2,6 +2,8 @@ import { api } from "@/utils/axios";
 
 export const getUser = (id: any) => api(`/users/${id}`, { method: "GET" });
 
+export const getUserByEmail = (email: any) => api(`/users/email/${email}`, { method: "GET" });
+
 export const putUser = (id: any, user:any) =>
   api(`/users/${id}`, {
     method: "PUT",
