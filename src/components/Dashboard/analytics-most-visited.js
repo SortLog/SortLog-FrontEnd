fbd2578ea62e36d825761aa9cc2758bff5bf805b
QZX91 <104272197+QZX91@users.noreply.github.com>
2022-11-22 18:38:56 +0800

    dashboard frontend

---
+++ src/components/Dashboard/analytics-most-visited.js
@@ -0,0 +1,114 @@
+import numeral from "numeral";
+import {
+  Box,
+  Card,
+  CardHeader,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import { ExternalLink as ExternalLinkIcon } from "../../icons/external-link";
+import { InformationCircleOutlined as InformationCircleOutlinedIcon } from "../../icons/information-circle-outlined";
+import { Scrollbar } from "../../layouts/scrollbar";
+
+const pages = [
+  {
+    date: "16/11/2022",
+    uniqueVisits: 8584,
+    activity: "Created folder Mango Syrup in store JM",
+    visitors: "Junyi Men",
+  },
+  {
+    date: "11/11/2022",
+    uniqueVisits: 648,
+    activity: "Removed 10 quantity of laptop from store JM",
+    visitors: "Junyi Men",
+  },
+  {
+    date: "06/11/2022",
+    uniqueVisits: 568,
+    activity: "Added 20 quantity of wooden spoon to store JM",
+    visitors: "Junyi Men",
+  },
+  {
+    date: "19/10/2022",
+    uniqueVisits: 12322,
+    activity: "Removed 50 quantity of CoffeeBean to store JM",
+    visitors: "Junyi Men",
+  },
+  {
+    date: "08/10/2022",
+    uniqueVisits: 11645,
+    activity: "Deleted folder plastic spoon in store JM",
+    visitors: "Junyi Men",
+  },
+  {
+    date: "22/09/2022",
+    uniqueVisits: 10259,
+    activity: "Added 30 quantity of straw to store JM",
+    visitors: "Junyi Men",
+  },
+];
+
+export const AnalyticsMostVisited = () => (
+  <Card>
+    <CardHeader
+      title="History"
+      action={
+        <Tooltip title="Refresh rate is 24h">
+          <InformationCircleOutlinedIcon sx={{ color: "action.active" }} />
+        </Tooltip>
+      }
+    />
+    <Scrollbar>
+      <Table sx={{ minWidth: 600 }}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Page Name</TableCell>
+            <TableCell>Visitors</TableCell>
+            {/* <TableCell>Unique page visits</TableCell> */}
+            <TableCell>Date</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {pages.map((page) => (
+            <TableRow
+              key={page.activity}
+              sx={{
+                "&:last-child td": {
+                  border: 0,
+                },
+              }}
+            >
+              <TableCell>
+                <Box
+                  sx={{
+                    alignItems: "center",
+                    display: "flex",
+                  }}
+                >
+                  <ExternalLinkIcon
+                    sx={{
+                      color: "action.active",
+                      cursor: "pointer",
+                    }}
+                  />
+                  <Typography sx={{ ml: 2 }} variant="body2">
+                    {page.activity}
+                  </Typography>
+                </Box>
+              </TableCell>
+              <TableCell>{page.visitors}</TableCell>
+              {/* <TableCell>{numeral(page.uniqueVisits).format("0,0")}</TableCell> */}
+              <TableCell>{page.date}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Scrollbar>
+  </Card>
+);
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,7 +1,79 @@
-import type { NextPage } from "next";
+// import { useEffect } from 'react';
+import Head from "next/head";
+import { Box, Button, Container, Grid, MenuItem, TextField, Typography } from "@mui/material";
+// import { AuthGuard } from '../../components/authentication/auth-guard';
+// import { DashboardLayout } from '../../components/dashboard/dashboard-layout';
+import { AnalyticsGeneralOverview } from "../../components/Dashboard/analytics-general-overview";
+import { AnalyticsMostVisited } from "../../components/Dashboard/analytics-most-visited";
+import { Reports as ReportsIcon } from "../../icons/reports";
+// import { gtm } from '../../lib/gtm';
 
-const Items: NextPage = () => {
-  return <p>Hello Items</p>;
+const Analytics = () => {
+  // useEffect(() => {
+  //   gtm.push({ event: 'page_view' });
+  // }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Dashboard</title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8,
+        }}
+      >
+        <Container maxWidth="xl">
+          <Box sx={{ mb: 4 }}>
+            <Grid container justifyContent="space-between" spacing={3}>
+              <Grid item>
+                <Typography variant="h4">Dashboard</Typography>
+              </Grid>
+              <Grid
+                item
+                sx={{
+                  alignItems: "center",
+                  display: "flex",
+                  m: -1,
+                }}
+              >
+                <Button
+                  startIcon={<ReportsIcon fontSize="small" />}
+                  sx={{ m: 1 }}
+                  variant="outlined"
+                >
+                  Reports
+                </Button>
+                <TextField defaultValue="week" label="Period" select size="small" sx={{ m: 1 }}>
+                  <MenuItem value="week">Last week</MenuItem>
+                  <MenuItem value="month">Last month</MenuItem>
+                  <MenuItem value="year">Last year</MenuItem>
+                </TextField>
+              </Grid>
+            </Grid>
+          </Box>
+          <AnalyticsGeneralOverview />
+          <Box sx={{ mt: 4 }}>
+            <Grid container spacing={4}>
+              <Grid item md={12} xs={12}>
+                <AnalyticsMostVisited />
+              </Grid>
+            </Grid>
+          </Box>
+        </Container>
+      </Box>
+    </>
+  );
 };
 
-export default Items;
+// Analytics.getLayout = (page) => (
+//   <AuthGuard>
+//     <DashboardLayout>
+//       {page}
+//     </DashboardLayout>
+//   </AuthGuard>
+// );
+
+export default Analytics;
